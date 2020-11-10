a81e42b4f4576668d41bfc6ab89b565909aaac20
Ian Havelock <ian@morrolan.com>
2020-11-10 17:48:24 +0000

    Added README and changelog

---
+++ changelog.md
@@ -0,0 +1,17 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [0.0.1] - 2020-11-10
+### Added
+- This CHANGELOG file to hopefully serve as an evolving example of a
+  standardized open source project CHANGELOG.
+- First release.
+- README now contains basic documentation.
+
+[0.0.1]: https://github.com/Morrolan/gormanian/releases/tag/v0.0.1
+
--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 from distutils.core import setup
+
 setup(
-    name = 'gormanian',
-    packages = ['gormanian'],
-    version = '0.1',
+    name='gormanian',
+    packages=['gormanian'],
+    version='0.0.1',
     license='MIT',
-    description = 'A simple library to convert a datetime date to use the superior Gormanian Calendar',
-    author = 'Ian Havelock',
-    author_email = 'ian@morrolan.com',
-    url = 'https://github.com/morrolan/gormanian',
-    download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-    keywords = ['Gormanian', 'Gormanuary', 'Dave Gorman'],
-    install_requires=[            # I get to this in a second
-        'validators',
-        'beautifulsoup4',
-    ],
+    description='A simple library to convert a datetime date to use the superior Gormanian Calendar',
+    author='Ian Havelock',
+    author_email='ian@morrolan.com',
+    url='https://github.com/Morrolan/gormanian',
+    download_url='https://github.com/Morrolan/gormanian/archive/v0.0.1.tar.gz',
+    keywords=['Gormanian', 'Gormanuary', 'Dave Gorman'],
+    install_requires=[],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
         'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Localization',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
